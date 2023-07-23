a792a9fdd64d23f63396cec3735b185dca67c57a
supardi_9512 <supardi@atomic.id>
2023-07-23 12:11:27 +0700

    add model product

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'product_category_id',
+        'name',
+        'description',
+        'price',
+        'tags',
+    ];
+
+    public function galleries() {
+        return $this->hasMany(ProductGallery::class, 'product_gallery_id', 'id');
+    }
+
+    public function category() {
+        return $this->belongsTo(ProductCategory::class, 'product_category_id', 'id');
+    }
+}
